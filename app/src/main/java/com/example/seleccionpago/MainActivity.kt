7c8c135f07299ec746df293580cd081155396dbd
Kevin <kevin.delbello@bue.edu.ar>
2024-10-17 21:05:43 -0300

    cambios de 17-10

--- app/src/main/java/com/example/seleccionpago/MainActivity.kt
+++ app/src/main/java/com/example/seleccionpago/MainActivity.kt
@@ -14,9 +14,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnNext = findViewById<Button>(R.id.btnNext)
+        val btnAddProduct = findViewById<Button>(R.id.btnAddProduct)
 
         btnNext.setOnClickListener {
             val intent = Intent(this, PantallaDos::class.java)
+            startActivity(intent)
+        }
+        btnAddProduct.setOnClickListener {
+            val intent = Intent(this, NewProduct::class.java)
             startActivity(intent)
         }
     }
